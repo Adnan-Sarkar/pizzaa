6b2d313cec79ef2cb130e750de22a346e219eb89
Aduvai1 <adnansarkar111@gmail.com>
2021-06-09 15:35:40 +0600

    Readme File Edit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import { Switch, Route } from "react-router-dom";
+import Home from "./pages";
+import About from "./pages/about";
+import Menu from "./pages/menu";
+import Footer from "./components/Footer";
+import Dropdown from "./components/Dropdown";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggle = () => {
+        setIsOpen(!isOpen);
+    };
+
+    useEffect(() => {
+        const hideMenu = () => {
+            if (window.innerWidth > 768 && isOpen) {
+                setIsOpen(false);
+                console.log("i resized");
+            }
+        };
+
+        window.addEventListener("resize", hideMenu);
+
+        return () => {
+            window.removeEventListener("resize", hideMenu);
+        };
+    });
+
+    return (
+        <>
+                <Navbar toggle={toggle} />
+                <Dropdown isOpen={isOpen} toggle={toggle} />
+                <Switch>
+                    <Route path="/" exact component={Home} />
+                    <Route path="/menu" component={Menu} />
+                    <Route path="/about" component={About} />
+                </Switch>
+                <Footer />
+        </>
+    );
 }
 
 export default App;
